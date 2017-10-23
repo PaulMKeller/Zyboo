72d51fc727f81463b9ba2935d50a7742ce441a06
Paul Keller <paul.keller.it@gmail.com>
2017-10-23 21:52:51 +0800

    Adding and Editing Service Charges
    Not complete as yet.

--- Zyboo/ChargesTableViewController.swift
+++ Zyboo/ChargesTableViewController.swift
@@ -13,6 +13,20 @@ class ChargesTableViewController: UITableViewController, TriggerServiceChargeSav
 
     var chargesObjs = [NSManagedObject]()
     
+    @IBAction func addTapped(_ sender: Any) {
+        prepareForSegue(segueIdentifier: "addNewCharge")
+    }
+    
+    @IBAction func editTapped(_ sender: UIBarButtonItem) {
+        if self.tableView!.isEditing {
+            self.setEditing(false, animated: true)
+            self.editButtonItem.title = "Edit"
+        } else {
+            self.setEditing(true, animated: true)
+            self.editButtonItem.title = "Done"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +36,11 @@ class ChargesTableViewController: UITableViewController, TriggerServiceChargeSav
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         loadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        loadData()
+        self.tableView!.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -89,6 +108,18 @@ class ChargesTableViewController: UITableViewController, TriggerServiceChargeSav
             print("Could not save. \(error), \(error.userInfo)")
         }
     }
+    
+    func prepareForSegue(segueIdentifier: String) {
+        self.performSegue(withIdentifier: segueIdentifier, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addNewCharge" {
+            let nextScene = segue.destination as! NewChargeViewController
+            let newCount = Int16(chargesObjs.count + 1)
+            nextScene.applicationOrder = newCount
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -96,23 +127,51 @@ class ChargesTableViewController: UITableViewController, TriggerServiceChargeSav
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+     */
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        // I need to change the application order if this row is moved.
+        reorderCharges(tableView: tableView)
+    }
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            
+            let managedContext = appDelegate.persistentContainer.viewContext
+            do {
+                let currentCharge = chargesObjs[indexPath.row] as! ServiceChargeObj
+                managedContext.delete(currentCharge)
+                chargesObjs.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                try managedContext.save()
+                reorderCharges(tableView: tableView)
+            } catch let error as NSError {
+                print("Could not fetch. \(error), \(error.userInfo)")
+            }
+        }
     }
-    */
-
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-        // I need to change the application order if this row is moved.
+    
+    
+    
+    func reorderCharges(tableView: UITableView) {
+        for cell in tableView.visibleCells as! [SettingChargeTableViewCell] {
+            //do someting with the cell here.
+            let cellDataObj = cell.cellDataObj
+            let indexRow:Int = tableView.indexPath(for: cell)!.row
+            let index:Int16 = Int16(indexRow)
+            cellDataObj.setValue(index, forKey: "applicationOrder")
+        }
+        saveData()
     }
 
     /*
@@ -130,7 +189,7 @@ class ChargesTableViewController: UITableViewController, TriggerServiceChargeSav
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
     }
     */
-
 }
--- Zyboo/NewChargeViewController.swift
+++ Zyboo/NewChargeViewController.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import CoreData
 
 class NewChargeViewController: UIViewController {
+    
+    var newChargeObj = NSManagedObject()
+    var applicationOrder:Int16 = 0
 
+    @IBOutlet weak var chargeName: UITextField!
+    @IBOutlet weak var percentageValue: UILabel!
+    @IBOutlet weak var stepperValue: UIStepper!
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        percentageValue.text = String(Int16(sender.value))
+    }
+    @IBOutlet weak var switchValue: UISwitch!
+
+    @IBAction func saveTapped(_ sender: Any) {
+        if chargeName.text != "" && stepperValue.value != 0 {
+            saveData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +37,29 @@ class NewChargeViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func saveData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        do {
+            let entityServiceChargeObj = NSEntityDescription.entity(forEntityName: "ServiceChargeObj", in: managedContext)
+            let newSrvChg = NSManagedObject(entity: entityServiceChargeObj!, insertInto: managedContext)
+            
+            newSrvChg.setValue(chargeName.text, forKey: "chargeName")
+            newSrvChg.setValue(Int16(stepperValue.value), forKey: "percentageCharge")
+            newSrvChg.setValue(applicationOrder, forKey: "applicationOrder")
+            newSrvChg.setValue(switchValue.isOn, forKey: "isOn")
+            
+            try managedContext.save()
+            _ = navigationController?.popViewController(animated: true)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
     
 
