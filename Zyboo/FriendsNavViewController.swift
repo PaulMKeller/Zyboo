1010e8d9eff006d4c28afa3d641d13e2ab61d611
Paul Keller <paul.keller@tigerspike.com>
2020-05-31 23:09:52 +0800

    Adding Friends View Controller
    Added controller and selcted and unselected images.

---
+++ Zyboo/FriendsNavViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FriendsNavViewController.swift
+//  Zyboo
+//
+//  Created by Paul Keller on 31/5/20.
+//  Copyright © 2020 PlanetKGames. All rights reserved.
+//
+
+import UIKit
+
+class FriendsNavViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
